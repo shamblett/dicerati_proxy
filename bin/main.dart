4946126b13877532794202bcd7d0fc3cf1abe34f
Steve Hamblett <steve.hamblett@linux.com>
2013-11-13 08:13:37 +0000

    Logging update

--- bin/main.dart
+++ bin/main.dart
@@ -29,7 +29,6 @@ void main() {
   String dateTime = now.toString();
   String logFileName = "$LOG_PATH$LOG_NAME-$dateTime.txt";
   Logger.root.onRecord.listen(new SyncFileLoggingHandler(logFileName));
-  //Logger log = new Logger('dicerati_proxy');
   
   /**
    * Startup message
